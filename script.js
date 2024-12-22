fad37e3f96c2bf1005a51ecd100dd2da4fc04872
gautamp1290 <gautamp1290@gmail.com>
2024-12-22 21:52:20 +0530

    created responsive navbar

---
+++ script.js
@@ -0,0 +1,20 @@
+
+
+
+
+function showSidebar(){
+    const sidebar=document.querySelector('.my-menu-class');
+    sidebar.style.display='flex';
+}
+
+
+
+function hideSideBar(){
+    const hidebar=document.querySelector('.my-menu-class');
+    hidebar.style.display='none';
+}
+
+
+
+
+  
